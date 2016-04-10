17aab1d6f19a2b88c170f4f618c7ebd752df3a37
Timo Weiß <info@timo-weiss.com>
2016-04-10 19:45:37 +0200

    codestyle (indent)

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,49 +6,49 @@ var yosay = require('yosay');
 module.exports = yeoman.Base.extend({
 
 
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the ' + chalk.red('seneca-environment') + ' generator!'
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the ' + chalk.red('seneca-environment') + ' generator!'
+        ));
 
-    var prompts = [{
-      type: 'list',
-      name: 'generator',
-      message: 'What do you want?',
-      default: 'system',
-      choices: [
-        {name: 'system', value: 'system'},
-        {name: 'base only', value: 'base'},
-        {name: 'service only', value: 'service'}]
-    }];
+        var prompts = [{
+            type: 'list',
+            name: 'generator',
+            message: 'What do you want?',
+            default: 'system',
+            choices: [
+                {name: 'system', value: 'system'},
+                {name: 'base only', value: 'base'},
+                {name: 'service only', value: 'service'}]
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
+        this.prompt(prompts, function (props) {
+            this.props = props;
 
-      if (this.props.generator === 'service') {
-        this.composeWith('seneca-environment:service');
-      } else if (this.props.generator === 'base') {
-        this.composeWith('seneca-environment:base');
-      } else {
-        this.composeWith('seneca-environment:service');
-        this.composeWith('seneca-environment:base');
-      }
+            if (this.props.generator === 'service') {
+                this.composeWith('seneca-environment:service');
+            } else if (this.props.generator === 'base') {
+                this.composeWith('seneca-environment:base');
+            } else {
+                this.composeWith('seneca-environment:service');
+                this.composeWith('seneca-environment:base');
+            }
 
-      done();
-    }.bind(this));
-  },
+            done();
+        }.bind(this));
+    },
 
-  writing: function () {
-    // this.fs.copy(
-    //   this.templatePath('dummyfile.txt'),
-    //   this.destinationPath('dummyfile.txt')
-    // );
-  },
+    writing: function () {
+        // this.fs.copy(
+        //   this.templatePath('dummyfile.txt'),
+        //   this.destinationPath('dummyfile.txt')
+        // );
+    },
 
-  install: function () {
-    // this.installDependencies();
-  }
+    install: function () {
+        // this.installDependencies();
+    }
 });
